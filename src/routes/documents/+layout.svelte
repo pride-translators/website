45d200edd8fad12ebb5dbdac1cbd099977bacf7f
pixelbracket <177620651+pixelbracket@users.noreply.github.com>
2024-09-13 19:00:51 +0900

    Add website title in meta tags

--- src/routes/documents/+layout.svelte
+++ src/routes/documents/+layout.svelte
@@ -21,8 +21,8 @@
 
 <svelte:head>
 	<title>프라이드 번역가 - {title}</title>
-	<meta property="og:title" content={title} />
-	<meta name="twitter:title" content={title} />
+	<meta property="og:title" content="프라이드 번역가 - {title}" />
+	<meta name="twitter:title" content="프라이드 번역가 - {title}" />
 </svelte:head>
 
 <style lang="scss">
