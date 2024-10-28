a4e4bd8cb5fc103ef0b9d6be49d7088db2cf9c67
pixelbracket <177620651+pixelbracket@users.noreply.github.com>
2024-10-28 19:21:49 +0900

    Add Google Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,4 +24,16 @@
 	<meta name="twitter:card" content="summary_large_image" />
 
 	<link rel="apple-touch-icon" href="{base}/logo512.png" />
+
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-DLGKJF181X"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'G-DLGKJF181X');
+	</script>
 </svelte:head>
