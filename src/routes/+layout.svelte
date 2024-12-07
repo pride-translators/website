6693273e40393a6ff0600750acb35abbaeae944b
evasquare <177620651+evasquare@users.noreply.github.com>
2024-12-07 18:37:50 +0900

    Fix og:url meta tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,7 +15,7 @@
 	<meta property="og:title" content="프라이드 번역가" />
 	<meta property="og:description" content="LGBTQIA+ 관련 자료를 한국어로 번역하는 사람들." />
 	<meta property="og:image" content="{base}/logo512.png" />
-	<meta property="og:url" content="https://pride-translators.github.io" />
+	<meta property="og:url" content="https://pride-translators.github.io/website" />
 
 	<meta name="twitter:title" content="프라이드 번역가" />
 	<meta name="twitter:description" content="LGBTQIA+ 관련 자료를 한국어로 번역하는 사람들." />
