e75d36f925d7db6372ae9d593d474ae1fc242d34
evasquare <177620651+evasquare@users.noreply.github.com>
2024-12-07 17:11:14 +0900

    Edit meta tags

--- src/routes/documents/+layout.svelte
+++ src/routes/documents/+layout.svelte
@@ -32,8 +32,13 @@
 
 <svelte:head>
 	<title>프라이드 번역가 - {title}</title>
-	<meta property="og:title" content="프라이드 번역가 - {title}" />
 	<meta name="description" content={excerpt ?? 'LGBTQIA+ 관련 자료를 한국어로 번역하는 사람들.'} />
+
+	<meta property="og:title" content="프라이드 번역가 - {title}" />
+	<meta
+		property="og:description"
+		content={excerpt ?? 'LGBTQIA+ 관련 자료를 한국어로 번역하는 사람들.'}
+	/>
 	<meta name="twitter:title" content="프라이드 번역가 - {title}" />
 	<meta
 		name="twitter:description"
